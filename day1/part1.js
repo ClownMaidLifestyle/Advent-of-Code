409fd89897ea0d7eb12bf7455fceec33f816a301
Elisa Smelly <e.edson@protonmail.ch>
2023-12-01 17:35:07 +0000

    day 1 setup

---
+++ day1/part1.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+//Variable bus stop
+let inputArray;
+let output;
+const input = require("./input");
+
+//Document Parse 
+
